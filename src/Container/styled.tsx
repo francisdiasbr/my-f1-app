8a56ecc07c9682c3c4eb8645a5aa3dc7c24b535c
Francis Dias <francisdiasbr@gmail.com>
2021-10-12 21:12:57 -0300

    Cria o styled do Container

---
+++ src/Container/styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  border: 1px solid rgba(0,0,0,0.2);
+  color: #005fdb;
+  font-size: 16px;
+  padding: 10px;
+  margin: 10px;
+  width: calc(100% - 40px);
+  ` 
