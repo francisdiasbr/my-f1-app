0a898de78393758b755c421a0dd875b09fbe2d4a
Francis Dias <francisdiasbr@gmail.com>
2021-10-12 21:13:33 -0300

    Cria o styled do Topbar

---
+++ src/Headings/styled.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const Heading1 = styled.h1`
+  color: #005fdb;
+  font-size: 16px;
+  margin: 0px;
+`
+
+export const TopbarContainer = styled.div`
+  
+  align-content: space-around;
+  align-items: center;
+  background-color:#ff8700;
+  display: flex;
+  flex-direction: row;
+  height: 40px;
+  padding: 10px;
+  width: calc(100% - 20px);
+`
