b79d69013cb876f52a9d0ee215975fac454a0b90
Francis Dias <francisdiasbr@gmail.com>
2021-10-12 21:24:30 -0300

    Cria o Topbar

---
+++ src/Topbar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Heading1, TopbarContainer } from '../Headings/styled'
+
+
+const Topbar = () => {
+  return (
+    <TopbarContainer>
+      <Heading1>F1 Stats - 2021 (until now!)</Heading1>
+      <Heading1>CAMPEONATO 2021</Heading1>
+      <Heading1>CIRCUITOS</Heading1>
+      <Heading1>PILOTOS</Heading1>
+    </TopbarContainer>
+  )
+}
+
+export default Topbar
