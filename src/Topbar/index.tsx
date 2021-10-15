7017a3bc6017ff26a08260ed83b08159e384aac9
Francis Dias <francisdiasbr@gmail.com>
2021-10-15 00:06:39 -0300

    Adiciona os links para as rotas do front no Topbar

--- src/Topbar/index.tsx
+++ src/Topbar/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import { Heading1, TopbarContainer } from '../Headings/styled'
-
+import { Link } from 'react-router-dom'
 
 const Topbar = () => {
   return (
     <TopbarContainer>
-      <Heading1>F1 Stats 2021</Heading1>
-      <Heading1>CAMPEONATO 2021</Heading1>
-      <Heading1>CIRCUITOS</Heading1>
+      <Link to='/'>
+        <Heading1>F1 Stats 2021</Heading1>
+      </Link>
+      <Link to='/apostas'>
+      <Heading1>APOSTAS</Heading1>
+      </Link>
+      <Link to='/circuitos'>
+        <Heading1>CIRCUITOS</Heading1>
+      </Link>
+      <Link to='/corridas'>
+        <Heading1>GRANDES PRÊMIOS</Heading1>
+      </Link>
+      <Link to='/pilotos'>
       <Heading1>PILOTOS</Heading1>
+      </Link>
+      <Link to='/equipes'>
+      <Heading1>EQUIPES</Heading1>
+      </Link>
+      <Link to='/calculadora'>
+      <Heading1>CALCULADORA</Heading1>
+      </Link>
     </TopbarContainer>
   )
 }
