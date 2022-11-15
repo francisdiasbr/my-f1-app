996bdb1a4906a6a912accf885e5aac27a5d7e23b
Francis Dias <francisdiasbr@gmail.com>
2022-11-15 19:25:30 -0300

    dados do cabeçalho de circuitos

--- components/NewCircuits/data.tsx
+++ components/NewCircuits/data.tsx
@@ -1,16 +1,22 @@
+import { FormInputProps } from "components/FormInput"
 
 export const titleStrings = {
-  circuitsTitle: 'Circuitos'
-}
-
-export const inputLabel = {
-  CountryLabel: 'País',
-  CityLabel: 'Cidade',
-  FastestLapTimeLabel: 'Volta + rápida (tempo)',
-  FastestLapYearLabel: 'Volta + rápida (ano)',
-  FastestLapDriverLabel: 'Volta + rápida (piloto)'
+  circuitsTitle: 'Circuitos',
+  driversTitle: 'Pilotos',
+  statisticsTitle: 'Estatísticas'
 }
 
 export const tableTitles = {
   circuitsTableTitle: 'Circuitos cadastrados'
 }
+
+export const circuitsFormInputs: Array<FormInputProps> = [
+  { name: 'CircuitLabel', value: 'Nome do circuito'},
+  { name: 'CountryLabel', value: 'País' },
+  { name: 'CityLabel', value: 'Cidade' },
+  { name: 'FastestLapTimeLabel', value: 'Volta + rápida (tempo)' },
+  { name: 'FastestLapYearLabel', value: 'Volta + rápida (ano)' },
+  { name: 'FastestLapDriverLabel', value: 'Volta + rápida (piloto)' }
+]
+
+export const circuitsTableHeaders = [{ value: 'Nome do circuito'}, {value: 'País'}, {value: 'Cidade' }, {value: 'Volta + rápida (tempo)' }, {value: 'Volta + rápida (ano)'}, {value: 'Volta + rápida (piloto)'} ]
