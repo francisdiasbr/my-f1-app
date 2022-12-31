645e591c33327e9860961bde93cb8920c70e3666
Francis Dias <francisdiasbr@gmail.com>
2022-12-31 20:06:30 -0300

    fix: small refactor on table and box

--- components/Box/props.tsx
+++ components/Box/props.tsx
@@ -3,17 +3,15 @@ import { PrensaEngineCSSProp } from '@xprog/prensa/types'
 export const boxContainerProps: PrensaEngineCSSProp = {
   align: ['column', 'center', 'top' ]
 }
-
 export const boxWrapProps: PrensaEngineCSSProp = {
   align: ['column', 'left', 'top' ]
 }
-
 export const boxAreaProps: PrensaEngineCSSProp = {
   backgroundColor: '$Neutral100',
+  borderRadius: '4px',
   height: '240px',
   width: '780px',
 }
-
 export const titleProps: PrensaEngineCSSProp = {
   align: ['row', 'left', 'top' ],
   color: '$Primary200',
--- components/Table/styled.tsx
+++ components/Table/styled.tsx
@@ -1,10 +1,45 @@
 import { PrensaEngine } from '@xprog/prensa'
-import { Block } from '@xprog/prensa'
 
-export const Title = PrensaEngine.styled('title', { color: 'black', fontSize: '16px' })
-export const Table = PrensaEngine.styled('table', {border: '1px solid', borderColor: '#767676', borderRadius: '4px', fontFamily: '$primary', width: '80%'})
-export const Tr = PrensaEngine.styled('tr', {})
-export const Thead = PrensaEngine.styled('thead', {})
-export const Th = PrensaEngine.styled('th', {color: '$Neutral800', fontFamily: '$primary', fontSize: '18px'})
-export const Tbody = PrensaEngine.styled('tbody', { fontFamily: '$primary' })
-export const Td = PrensaEngine.styled('td', { border: '1px solid', borderColor: '#767676', borderRadius: '4px', fontFamily: '$primary'})
+export const Table = PrensaEngine.styled(
+  'table',
+  {
+    border: '1px solid',
+    color: '$Neutral100',
+    borderRadius: '4px',
+    fontFamily: '$primary',
+    width: '800px'
+  }
+)
+export const Tr = PrensaEngine.styled(
+  'tr',
+  {}
+)
+export const Thead = PrensaEngine.styled(
+  'thead',
+  {}
+)
+export const Th = PrensaEngine.styled(
+  'th',
+  { 
+    color: '$Neutral800', 
+    fontFamily: '$primary', 
+    fontSize: '18px',
+    fontWeight: '400'
+  }
+)
+export const Tbody = PrensaEngine.styled(
+  'tbody',
+  {
+    fontFamily: '$primary'
+  }
+)
+export const Td = PrensaEngine.styled(
+  'td',
+  { 
+    border: '1px solid', 
+    borderColor: '$Neutral300', 
+    borderRadius: '4px', 
+    fontFamily: '$primary',
+    height: '18px'
+  }
+)
