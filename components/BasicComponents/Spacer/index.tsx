31554d990e4caef141d58409b1ff2f95d3eaa74f
Francis Dias <francisdiasbr@gmail.com>
2023-01-01 19:39:29 -0300

    feat: add spacer

---
+++ components/BasicComponents/Spacer/index.tsx
@@ -0,0 +1,12 @@
+import { Block } from '@xprog/prensa'
+import { PrensaEngineCSSProp } from '@xprog/prensa/types'
+import {spacerProps} from './styles'
+
+const Spacer = () => {
+  
+  return (
+    <Block css={spacerProps}></Block>
+  )
+}
+
+export default Spacer
---
+++ components/BasicComponents/Spacer/styles.ts
@@ -0,0 +1,11 @@
+import { PrensaEngineCSSProp } from '@xprog/prensa/types'
+
+export const spacerProps: PrensaEngineCSSProp = {
+  borderBottom: '1px solid gray',
+  mb: '16px',
+  mt: '16px',
+  width: 'auto',
+  '@lg': {
+    maxWidth: '700px',
+  }
+}
