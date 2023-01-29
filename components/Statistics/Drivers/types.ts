8f43576297d25503fcc0f81a529b6baadc278e3b
Francis Dias <francisdiasbr@gmail.com>
2023-01-29 20:35:47 -0300

    add id no type do input

--- components/Statistics/Drivers/types.ts
+++ components/Statistics/Drivers/types.ts
@@ -1,6 +1,7 @@
 import { FormInputType } from 'components/BasicComponents/FormInput/types'
 
 export type driversFormValuesType = {
+  driverId?: string,
   driverNameLabel?: FormInputType,
   driverBirthLabel?: FormInputType,
   driverCountryLabel?: FormInputType
